6dbdc5ff9540f3dd447a7a3bb4e13e44597e0994
Moses <lazaromoses77@gmail.com>
2020-07-15 23:12:35 +0300

    Updates

---
+++ precache-manifest.405a468882cff831fae0349a701cd3a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c92013750e15424a001520f46cb5b6",
+    "url": "/fodie_app/index.html"
+  },
+  {
+    "revision": "cea09d5370e42ff3bc44",
+    "url": "/fodie_app/static/css/main.fd3a7839.chunk.css"
+  },
+  {
+    "revision": "ac84f3584a169c95ec6b",
+    "url": "/fodie_app/static/js/2.baa8f678.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fodie_app/static/js/2.baa8f678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea09d5370e42ff3bc44",
+    "url": "/fodie_app/static/js/main.8da6a005.chunk.js"
+  },
+  {
+    "revision": "8bff53a361865dfdf39b",
+    "url": "/fodie_app/static/js/runtime-main.8417a1d9.js"
+  }
+]);
